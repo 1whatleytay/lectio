af10bc5042600a0746f8fc5a164c0dda6afa4e63
1whatleytay <1whatleytay@hdsb.ca>
2019-05-04 20:48:41 -0400

    New Recording API

--- src/components/Recording.vue
+++ src/components/Recording.vue
@@ -7,61 +7,56 @@
 </template>
 
 <script>
-import Artyom from 'artyom.js'
-
-const artyomDebug = true;
-
 export default {
   name: 'Recording',
 
+  props: [ 'short' ],
+
   data() {
     return {
-      artyom: new Artyom(),
+      recognition: new webkitSpeechRecognition(),
       isFinal: true,
-      text: ''
+      text: '',
+      currentTimeout: null,
     }
   },
 
   mounted() {
-    // Init Artyom
-    this.artyom.initialize({
-      // English
-      lang: 'en-US',
-      // Enable Speach Recognition
-      listen: true,
-      // Listen Constantly
-      continuous: true,
-      // Debug Console Output
-      debug: artyomDebug,
-      // Speed at which artyom.say() speaks. Not really needed.
-      speed: 1,
-      // Allows you to use remoteProcessorService, which is nice.
-      mode: 'remote'
-    }).then(() => {
-      this.isFinal = true;
-
-      // Set the listener.
-      this.artyom.remoteProcessorService(this.listen)
-    })
+    this.recognition.lang = 'en-US'
+    this.recognition.continuous = true
+    this.recognition.interimResults = true
+    this.recognition.maxAlternatives = 1
+    this.recognition.onresult = this.listen
   },
 
   methods: {
     listen(data) {
-      if (!this.isFinal) {
-        this.isFinal = data.isFinal
-        this.text = data.text
+      const last = data.results[data.results.length - 1]
+      this.text = last[0].transcript
 
-        if (this.isFinal) {
-          this.$emit('finished', this.text)
-        }
+      if (last.isFinal) {
+        this.isFinal = true
+        this.close()
+        this.$emit('finished', this.text)
       }
     },
 
     startRecording() {
       this.isFinal = false
       this.text = ''
+      this.recognition.start()
       this.$emit('record')
-    }
+      if (this.short) {
+        this.currentTimeout = setTimeout(this.close, 5000)
+      }
+    },
+
+    close() {
+      if (this.currentTimeout) {
+        clearTimeout(this.currentTimeout)
+      }
+      this.recognition.stop()
+    },
   }
 }
 </script>
