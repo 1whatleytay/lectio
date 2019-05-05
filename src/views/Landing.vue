55e0dc3bc33691b15b8acffd0455ba3b0b8a7f1f
Viktor Korolyuk <viktor.korolyuk@gmail.com>
2019-05-04 22:18:35 -0400

    Create Landing.vue

---
+++ src/views/Landing.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+  <div class="" id="overlay">
+    <img src="/ui/wave.svg"/>
+  </div>
+
+  </div>
+</template>
+
+<style>
+
+#overlay{
+  position: absolute;
+  width: 100%;
+  min-height: 10em;
+  top:0;
+
+  
+}
+
+</style>
