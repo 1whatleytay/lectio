3a241268ecaff47ee659026eab577955722e0190
Viktor Korolyuk <viktor.korolyuk@gmail.com>
2019-05-05 05:37:11 -0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,26 @@
 <template>
-  <div id="app">
-    <div v-if="navState.state !== 'NoShow'">
-      <div v-if="navState.state === 'Normal'" class="shadow text-4xl tts" :class="navState.color" id="nav">
-        Lectio
-      </div>
-      <div v-if="navState.state === 'Comfy'" class="shadow flex" :class="navState.color" id="nav">
-        <div class="w-1/2 text-4xl text-left pl-4">Lectio</div>
-        <div class="w-1/2 text-xl mt-2 text-right pr-4">Welcome {{ navState.user }}</div>
-      </div>
+<div id="app">
+  <div v-if="navState.state !== 'NoShow'">
+    <div v-if="navState.state === 'Normal'" class="shadow text-4xl tts" :class="navState.color" id="nav">
+      Lectio
     </div>
-    <router-view />
-    <div class="py-8"></div>
+    <div v-if="navState.state === 'Comfy'" class="shadow flex" :class="navState.color" id="nav">
+      <div class="w-1/2 text-4xl text-left pl-4">Lectio</div>
+      <div class="w-1/2 text-xl mt-2 text-right pr-4">Welcome {{ navState.user }}</div>
+    </div>
   </div>
+  <router-view />
+  <div class="py-8"></div>
+</div>
 </template>
 
 <script>
-import { NavState } from './script/nav.js'
-import {requestSpeak} from './script/text_to_speech.js'
+import {
+  NavState
+} from './script/nav.js'
+import {
+  requestSpeak
+} from './script/text_to_speech.js'
 
 import axios from 'axios'
 
@@ -38,41 +42,39 @@ export default {
     });
 
     document.querySelectorAll(".tts").forEach(el => {
-  el.onmouseenter = mouseEvent => {
-    let elem = document.elementFromPoint(mouseEvent.x, mouseEvent.y);
-    let data = {
-      elem,
-      x: mouseEvent.x,
-      y: mouseEvent.y,
+      el.onmouseenter = mouseEvent => {
+        let elem = document.elementFromPoint(mouseEvent.x, mouseEvent.y);
+        let data = {
+          elem,
+          x: mouseEvent.x,
+          y: mouseEvent.y,
+        };
+
+        // After a certain amount of time check if the current hovered element is the same as the previously hovered element
+        // if so, do the speech
+        setTimeout(() => {
+          console.log("mouseEvent");
+          let hoveringElement = document.elementFromPoint(mousePosX, mousePosY);
+          if (hoveringElement == data.elem) {
+            // debugger;
+            requestSpeak(elem.innerText, event => {
+              elem.classList.remove("enabled");
+            })
+            elem.classList.add("enabled");
+          }
+        }, 1000);
+      };
+    });
+
+    window.mousePosX = -1;
+    window.mousePosY = -1;
+
+    window.onmousemove = mouseEvent => {
+      window.mousePosX = mouseEvent.x;
+      window.mousePosY = mouseEvent.y;
     };
-
-    // After a certain amount of time check if the current hovered element is the same as the previously hovered element
-    // if so, do the speech
-    setTimeout(() => {
-      console.log("mouseEvent");
-      let hoveringElement = document.elementFromPoint(mousePosX, mousePosY);
-      if (hoveringElement == data.elem) {
-        // debugger;
-        requestSpeak(elem.innerText, event => {
-          elem.classList.remove("enabled");
-        })
-        elem.classList.add("enabled");
-      }
-    }, 1000);
-  };
-});
-
-window.mousePosX = -1;
-window.mousePosY = -1;
-
-window.onmousemove = mouseEvent => {
-  window.mousePosX = mouseEvent.x;
-  window.mousePosY = mouseEvent.y;
-};
-    // debugger;
   },
-  beforeMount() {
-  },
+  beforeMount() {},
 }
 
 // window.onload =
@@ -85,22 +87,22 @@ window.onmousemove = mouseEvent => {
 </style>
 
 <style>
-  #app{
-    text-align: center;
-  }
+#app {
+  text-align: center;
+}
 
-  #nav{
-    color: white;
-    padding: 0.5em;
-    margin-bottom: 1em;
-  }
+#nav {
+  color: white;
+  padding: 0.5em;
+  margin-bottom: 1em;
+}
 
-  @font-face {
-    font-family: 'Dyslexie';
-    src: url('/fonts/Dyslexie_Regular_149888.ttf');
-  }
+@font-face {
+  font-family: 'Dyslexie';
+  src: url('/fonts/Dyslexie_Regular_149888.ttf');
+}
 
-  div {
-    font-family: 'Dyslexie';
-  }
+div {
+  font-family: 'Dyslexie';
+}
 </style>
