0cf8d18a691ff2ad293692a1be34e38a1368ae51
YT-Main <t.yash2003@gmail.com>
2019-05-05 00:11:05 -0400

    check for overlay

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,21 +1,25 @@
 <template>
-  <div>
-  <div class="" id="overlay">
-    <img src="/ui/wave.svg"/>
-  </div>
-
-  </div>
+<body>
+	<div class="test">
+		<div class="test2"></div>
+	</div>
+</body>
 </template>
-
-<style>
-
-#overlay{
-  position: absolute;
-  width: 100%;
-  min-height: 10em;
-  top:0;
-
-  
-}
-
+<style type="text/css">
+	.test{
+		background: url("/ui/wave.svg");
+		width: auto;
+		height: 100vh;
+    background-repeat: no-repeat;
+    background-size:100%;
+    overflow: hidden;
+    margin-top:-9.5em;
+	}
+	.test2{
+		width: 20%;
+		height: 20vh;
+		background: black;
+		margin-top: 40vh;
+		margin-left: 40%;
+	}
 </style>
